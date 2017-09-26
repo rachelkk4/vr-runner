51e2bf8d5f651581ebdb11b148091829529fe771
rakitch <rachelkk4@gmail.com>
2017-09-26 08:30:16 -0400

    Added score counting GUI
    Implemented the hit counter and miss counter.

--- vr-runner/Assets/Game/Assets/Scripts/CubeCustom.cs
+++ vr-runner/Assets/Game/Assets/Scripts/CubeCustom.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeCustom : MonoBehaviour {
 
     float RandomSpeed;
     public GameObject explode;
 
-    // Use this for initialization
+    public Text missCountText;
+    public static int missCount;
+
+    public Text hitCountText;
+    public static int hitCount;
+
+    // New cube targets should fade in and be red in color.
     void Start () {
         StartCoroutine(FadeIn());
         gameObject.GetComponent<Renderer>().material.color = Color.red;
-	}
 
+        RandomSpeed = Random.Range(3, 6);  // initialize cube travel speed
+
+        missCountText = GameObject.FindGameObjectWithTag("MissText").GetComponent<Text>();
+        hitCountText = GameObject.FindGameObjectWithTag("HitText").GetComponent<Text>();
+    }
+
+    // Fade in script
     IEnumerator FadeIn()
     {
         for (float f = 0f; f <= 1; f += 0.05f)
@@ -25,8 +38,8 @@ public class CubeCustom : MonoBehaviour {
     }
 
 	// Update is called once per frame
+    // The cube moves linearly towards the player and rotates as it moves
 	void Update () {
-        RandomSpeed = Random.Range(3, 6);
         transform.Translate(Time.deltaTime * RandomSpeed, 0, 0, Space.World);
 
         transform.Rotate(Vector3.one, 30 * Time.deltaTime);
@@ -38,10 +51,23 @@ public class CubeCustom : MonoBehaviour {
         Destroy(gameObject);
     }
 
+    void increaseMissCount()
+    {
+        missCount++;
+        missCountText.text = string.Format("Misses: {0:00}", missCount);
+    }
+
+    public void increaseHitCount()
+    {
+        hitCount++;
+        hitCountText.text = string.Format("Hits: {0:00}", hitCount);
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "plane")
         {
+            increaseMissCount();
             Explode();
         }
     }
